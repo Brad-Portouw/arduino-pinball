bc1986ef96a041e42c0d7810820b9b8baccc291b
Brad-Portouw <bportouw337@gmail.com>
2025-03-06 14:46:36 -0700

    Added preliminary code to test a rollover switch. Seemed to work well, but may need some further testing for accuracy.

---
+++ code/component_test_code/rollover_switch.py
@@ -0,0 +1,38 @@
+# rollover_switch.py
+# Written by: Brad Portouw
+# Date: 3/6/25
+
+# Code to interface with an Arduino Uno to read the triggering of a rollover switch, a common switch that is used in pinball machines.
+# Assumption with the switch is that it will work in a normally-open contact (NO)
+# Switch used:
+# https://www.betsonparts.com/amusement-redemption/sub-mini-switch-db5-style.html
+
+# Typically there are many switches being used in the pinball machine at one time, but only one pinball.
+# This may mean that the switches can be run in parallel all being connected to one pin because they should only be triggered one at a time.
+#
+# Import packages
+import pyfirmata
+import time
+
+# Connect to the board through 'COM3"
+board = pyfirmata.Arduino('COM3')
+
+# Iterator which will loop to keep track of inputs and outputs
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+# pin 10 will check to see if the switch has closed, while pin 13 will light a LED to show the switch closing.
+switch_input = board.get_pin('d:10:i')
+LED = board.get_pin('d:13:o')
+
+while True:
+    switch = switch_input.read()
+    if switch is True:
+        LED.write(1)
+    else:
+        LED.write(0)
+    # Note on refresh rate, may need to be higher (check more than 10 times a second) depending upon how long the switch is closed for.
+    time.sleep(0.1)
+
+
+
